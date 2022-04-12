73265fa9621547130e9d1f7e6956c450f181aeeb
Joseph Madenjian <joseph.madenjian@gmail.com>
2022-04-12 10:15:51 -0400

    'monday'

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import Button from './components/Button';
+import Nav from "./components/Nav";
+import KekTable from './views/Kektable';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+export default class App extends Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            count: 0,
+            students: []
+        }
+    }
+
+    componentDidMount(){
+        console.log('App mounted')
+        fetch('https://kekambas-bs.herokuapp.com/kekambas')
+            .then(res => res.json())
+            .then(data => {
+                console.log(data)
+                let students = data
+                this.setState({students})
+                
+            })
+    }
+
+    handleButtonClick = (step) => {
+        let newCount = this.state.count + step;
+        this.setState({
+            count: newCount
+        })
+    }
+
+    render(){
+        const myButtonSteps = [1, 10, 100, 1000, 10000, 100000]
+        console.log('App rendered')
+        return (
+            <>
+                <Nav />
+                <div className='container'>
+                    <h1>Hello World!</h1>
+                    <h4 className='text-center'>Current Count: {this.state.count}</h4>
+                    <div className='d-flex justify-content-around'>
+                        {myButtonSteps.map((step, i) => <Button key={i} step={step} handleClick={this.handleButtonClick} />)}
+                    </div>
+                    <KekTable students={this.state.students}/>
+                </div>
+            </>
+        )
+    }
 }
-
-export default App;
---
+++ src/components/Button.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Button extends Component {
+
+
+    render() {
+        return (
+            <button onClick={() => this.props.handleClick(this.props.step)} className='btn btn-primary'>+{this.props.step}</button>
+        )
+    }
+}
